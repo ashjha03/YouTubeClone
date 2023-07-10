0ab471a7808db2ff7eb2449830c9480fc6c1a44e
Ashish Jha <88213707+ashjha03@users.noreply.github.com>
2023-07-10 20:56:26 +0530

    Main Page referenced video link changed

--- src/component/videos/Videos.jsx
+++ src/component/videos/Videos.jsx
@@ -1,36 +1,32 @@
-import {useEffect, useState} from "react"
-import fetchFromAPI from '../../fetchFromAPI/fetchFromAPI'
-import VideoItem from "./VideoItem"
-import Spinner from "../../layout/Spinner"
+import { useEffect, useState } from "react";
+import fetchFromAPI from "../../fetchFromAPI/fetchFromAPI";
+import VideoItem from "./VideoItem";
+import Spinner from "../../layout/Spinner";
 
 const Videos = () => {
-    const [VideoList, setVideoList] = useState([])
-    const requested_url = 'search?relatedToVideoId=idpPTUyNP5c&part=id%2Csnippet&type=video&maxResults=10'
+  const [VideoList, setVideoList] = useState([]);
+  const requested_url =
+    "search?relatedToVideoId=Z0hwjftStI4&part=id%2Csnippet&type=video&maxResults=10";
 
-    const updateData = async () => {
-        const data = await fetchFromAPI(requested_url)
-        setVideoList(data.items)
-    }
+  const updateData = async () => {
+    const data = await fetchFromAPI(requested_url);
+    setVideoList(data.items);
+  };
 
-    useEffect(() => {
-        updateData()
-        // eslint-disable-next-line
-    }, [])
+  useEffect(() => {
+    updateData();
+    // eslint-disable-next-line
+  }, []);
 
-    if (!VideoList) 
-        return <Spinner />
-    
-    return (
-        <div className="grid lg:grid-cols-4 gap-5 p-3 lg:pt-5 bg-[#272727] text-white">
-            {
-            VideoList.map(data => {
-                return <VideoItem key={
-                        data.snippet.title
-                    }
-                    video={data}/>
-            })
-        } </div>
-    )
-}
+  if (!VideoList) return <Spinner />;
 
-export default Videos
+  return (
+    <div className="grid lg:grid-cols-4 gap-5 p-3 lg:pt-5 bg-[#272727] text-white">
+      {VideoList.map((data) => {
+        return <VideoItem key={data.snippet.title} video={data} />;
+      })}{" "}
+    </div>
+  );
+};
+
+export default Videos;
